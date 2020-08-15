b93c93c8247803903aecbd184e0bfd29355704cc
Rhythm Bhiwani <rhythmbhiwani@gmail.com>
2020-08-15 22:07:48 +0530

    Lec - 4 Added

---
+++ Lec - 4 Basics.py
@@ -0,0 +1,32 @@
+# Print Function
+print('Hi There Logic Decode')
+print(5+3)
+
+# This is a comment
+
+#Variable
+name = "RHYTHM BHIWANI"
+print(name)
+
+name = "LOGIC DECODE"
+print(name)
+
+# Identifiers
+myname = "RHYTHM BHIWANI"
+print(myname)
+
+#RUles for Identifiers
+# It should start with A-Z a-z _
+#Correct Names
+rhythm
+_rhythm
+Rhythm
+rhythm1
+my_name
+
+#incorrect names
+5rhythm
+@rhythm
+my name
+rhythm$
+while = 'Hello'
