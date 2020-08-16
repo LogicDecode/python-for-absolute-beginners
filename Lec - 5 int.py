5dd736b7ff930fe0ef506266cda5b08a5effe423
Rhythm Bhiwani <rhythmbhiwani@gmail.com>
2020-08-17 01:30:24 +0530

    Lec - 5 Added

---
+++ Lec - 5 int.py
@@ -0,0 +1,10 @@
+a = 5
+print(type(a)) #int
+print(5+3) #Addition
+print(8-6) #Subtraction
+print(4*3) #Multiplication
+print(8/2) #Division
+print(5/2) #Division
+print(5//2) #Floor Division
+print(5%2) #Modulus
+print(2**3) #Power
